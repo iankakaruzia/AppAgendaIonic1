20cdb8baa3b05cdf0b544abdd55c789d7acfe46a
Ianka Karúzia <iankakaruzia@hotmail.com>
2017-07-19 16:26:43 -0300

    First Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,75 @@
+angular.module('app.controllers', [])
+  
+.controller('agendaCtrl', ['$scope', '$stateParams', '$ionicModal', 'agendaApi', function ($scope, $stateParams, $ionicModal, agendaApi) {
+	var contatos = [];
+	$scope.permitirExcluir = false;
+	$scope.favorito = false;
+
+	function obterContatos() {
+		contatos = agendaApi.getContatos();
+		$scope.contatos = contatos;
+
+		return contatos;
+	} 
+
+	$scope.salvarContato = function(contato) {
+		agendaApi.addContato(contato);
+		obterContatos();
+
+		$scope.closeModal();
+	}
+
+	$scope.excluirContato = function(contato) {
+		agendaApi.deleteContato(contato);
+
+		obterContatos();
+	}
+
+	$scope.filtrarContatosFavoritos = function() {
+		contatos = obterContatos();
+
+		if ($scope.favorito == true) {
+			contatos = contatos.filter(function(contatos) {
+				return contatos.favorito == true;
+			});
+
+			$scope.contatos = contatos;
+		}
+	}
+
+	obterContatos();
+
+	$scope.habilitarExclusao = function(){
+		$scope.permitirExcluir = !$scope.permitirExcluir;
+	}
+
+
+	$scope.abrirTelaContato = function() {
+		$scope.contato = {nome:'', telefone:'', favorito:false};
+		$scope.openModal();
+	}
+
+	$scope.cancelarContato = function() {
+		$scope.closeModal();
+	}
+
+	//Modal
+	$ionicModal.fromTemplateUrl('contato.html', {scope: $scope, animation: 'slide-in-up'}).then(function(modal) {
+    	$scope.modal = modal;
+  	});
+
+	$scope.openModal = function() {
+	  $scope.modal.show();
+	};
+
+	$scope.closeModal = function() {
+	  $scope.modal.hide();
+	};
+
+}])
+   
+.controller('sobreCtrl', ['$scope', '$stateParams', function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,27 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('agenda', {
+    url: '/agenda',
+    templateUrl: 'templates/agenda.html',
+    controller: 'agendaCtrl'
+  })
+
+  .state('sobre', {
+    url: '/sobre',
+    templateUrl: 'templates/sobre.html',
+    controller: 'sobreCtrl'
+  })
+
+$urlRouterProvider.otherwise('/agenda')
+
+
+});
