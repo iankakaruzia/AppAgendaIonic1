798daa4b066300341ce59822c2715c21815b3265
Ianka Karúzia <iankakaruzia@hotmail.com>
2017-07-20 01:10:26 -0300

    Added animation

--- www/js/app.js
+++ www/js/app.js
@@ -14,7 +14,7 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
 })
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function($ionicPlatform, $state, $ionicHistory, $ionicPopup) {
+  $ionicPlatform.ready(function($state, $ionicHistory, $ionicPopup) {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
